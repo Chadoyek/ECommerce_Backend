8f5e6a15ace66645491b624ee233a94c585feeb5
Chadi hoayeK <chadihoayek@Chadis-MacBook-Pro.local>
2024-06-03 11:48:59 -0700

    code fixes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -1,16 +1,80 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
+
+
+const includeProducts = [
+  {
+      model: Product,
+      as: 'tagged_products',
+      attributes: [
+          'id',
+          'product_name',
+          'price',
+          'stock',
+          'category_id'
+      ]
+  }
+]
+
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+    Tag.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: [
+          'id',
+          'product_name',
+          'price',
+          'stock',
+          'category_id'
+        ]
+      },
+      includeProducts
+    ]
+  })
+    .then(dbData => res.json(dbData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+        id: req.params.id
+    },
+    include: [
+      { 
+        model: ProductTag,
+        as: 'tagged_products',
+        attributes: [
+          'id',
+          'product_id',
+          'tag_id'
+        ]
+      },
+      includeProducts
+    ]
+})
+    .then(dbData => {
+        if (!dbData) {
+            res.status(404).json({ message: 'No tag found with this id' });
+            return;
+        }
+        res.json(dbData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
